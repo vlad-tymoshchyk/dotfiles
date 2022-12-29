f5fff41b7d37b3fd94dd980d569d8b2ed4d47b06
Vlad Tymoshchyk <vtymoshchyk@gmail.com>
2022-12-29 23:15:43 +0200

    feat: make start-setup not executable

--- setup/start-setup.sh
+++ setup/start-setup.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
-
-
 sudo apt-get update
-# sudo apt-get upgrade --yes
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 
 sudo apt-get install --yes software-properties-common git ansible
